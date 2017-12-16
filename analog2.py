cc567421d121875d60574e7e6714e5b32f8d8b3f
Alejandro Bravo <alejandrobravotrabajo@gmail.com>
2017-12-16 19:57:16 +0100

    Diccionario con IPs y ultima fecha de ataque

--- analog2.py
+++ analog2.py
@@ -5,14 +5,22 @@ formato = "%b %d %H:%M:%S"
 fecha = []
 dic = {}
 valores = []
+contador = 0
 fichero_origen = open('auth.log', 'r')
 for linea in fichero_origen.readlines():
 	if 'authentication failure' in linea:
 		fecha.append(datetime.strptime(linea[0:15],formato))
 		spl=linea.split()
 		ip=spl[13]
-		clave=ip[6:]
-		dic[clave]=linea[0:15]+','
+		clave = ip[6:]
+		valores.append(linea[0:15])
+		valores.reverse()
+		for x in range(len(linea)):
+			dic[clave]= []
+		for ele in valores:
+			if ele in linea and clave in linea:
+				dic[clave].append(ele)
+
 
 				
 		
