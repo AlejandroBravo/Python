209d6385e74a331d638794e4f4982e7196e767ed
Alejandro Bravo <alejandrobravotrabajo@gmail.com>
2018-01-11 01:19:59 +0100

    Baqueteos y pruebas

--- Nataques.py
+++ Nataques.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 from datetime import datetime
+
+numero=3
+contador=0
+fecha1='Nov 24 05:45:22'
+fecha2='Nov 24 07:13:33'
+
 def ana():
 	f = open ('auth.log', 'r')
 	ha={}
+	ataques=[]
 	for lin in f.readlines():
-	    	if 'authentication failure' in lin:
+		if 'authentication failure' in lin:
 			li=lin.split()
 			sfe = li[0]+li[1]+li[2]
 			fe = datetime.strptime(sfe,'%b%d%H:%M:%S')
@@ -14,7 +21,12 @@ def ana():
 					ip = ele[6:]
 			if ip not in ha:
 				ha[ip]=[]
-			ha[ip].append(fe)				
+			ha[ip].append(fe)
+	for lin in f.readlines():
+		
+		while contador <= numero:
+			ataques.append()
+						
 	f.close()
 	for ip in ha:
 		ha[ip].sort(reverse=True)
